498c09b5cfad01619ecaad85f8be64a8e4a63bae
KaKa_Xie <mrxiehuaqiang@163.com>
2021-02-20 19:12:59 +0800

    根据vue-cli梳理代码逻辑

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 # 介绍
 
+## 第三方库
+
 - [execa 中文说明](http://abloz.com/tech/2018/08/21/nodejs-execa/)
+
+  > execa 是可以调用 shell 和本地外部程序的 javascript 封装。会启动子进程执行。支持多操作系统，包括 windows。如果父进程退出，则生成的全部子进程都被杀死。
+
+- [commander](https://www.npmjs.com/package/commander)
+
+  > 命令行工具
+
+- [minimist](https://www.npmjs.com/package/minimist)
+
+  > 格式化 arg 参数
+
+- [semver](https://www.npmjs.com/package/semver)
+
+  > 获取版本号，可以直接`npm link semver`，执行 `semver v1.2.3` => `1.2.3`
+
+## 文档
+
+[Node.js 中文网](http://nodejs.cn/api/child_process.html#child_process_child_process_spawn_command_args_options)
+
+## 知识点
+
+1. \_\_dirname
+
+    ```text
+    __dirname： 获得当前执行文件所在目录的完整目录名 __filename：
+    获得当前执行文件的带有完整绝对路径的文件名
+    process.cwd()：获得当前执行node命令时候的文件夹目录名 ./：
+    不使用require时候与process.cwd()一样，使用require时候与__dirname一样
+    ```
+
+2. npm run test
+
+```json
+"scripts": {
+  "test": "cross-env-shell VUE_CLI_TEST=true kaka create",
+},
+```
+
+当执行 `npm run test hello` 或者 `npm run test -- hello`时会执行成`cross-env-shell VUE_CLI_TEST=true kaka create hello`
--- bin/kaka.js
+++ bin/kaka.js
@@ -1,4 +1,53 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk')
-console.log(chalk.red('\n × Template does not exit!'))
+const { chalk, semver } = require('@vue/cli-shared-utils')
+const program = require('commander')
+const minimist = require('minimist')
+
+program
+  .version(`kaka-cli ${require('../package').version}`, '-v, --vers')
+  .usage('<command> [options]')
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by vue-cli-service')
+  .action((name, option, cmd) => {
+    console.log('name: ', name)
+    const options = cleanArgs(cmd)
+    console.log('options: ', options)
+    if (minimist(process.argv.slice(3))._.length > 1) {
+      console.log(chalk.yellow('\n Info: You provided more than one argument. The first one will be used as the app\'s name, the rest are ignored.'))
+    }
+    require('../lib/create')(name, options)
+  })
+// 最后执行
+program.parse(process.argv)
+
+const options = program.opts()
+
+/**
+ * 将连字符命名的转为驼峰格式
+ * @param {*} str  x-y-z => xYZ
+ */
+function camelize (str) {
+  return str.replace(/-(\w)/g, (match, group) => group ? group.toUpperCase() : '')
+}
+
+/**
+ * commander将Command对象本身作为选项传递
+ * 将实际的选项提取到一个新对象中(args)。
+ * @param {Object} cmd  command实例
+ */
+function cleanArgs (cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  console.log('cleanArgs => args: ', args)
+  return args
+}
